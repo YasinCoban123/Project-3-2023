8a858161181cc38ae886033a95297583badc1208
danielkuiper1 <9015587@student.tcrmbo.nl>
2023-03-17 13:43:04 +0100

    Over Ons Page

--- project3/overons.php
+++ project3/overons.php
@@ -9,7 +9,65 @@
 </head>
 <body>
 <?php include 'navbar.php'; ?>
-<section class="overons">
+<main class="OverOnsMain">
+        <section class="overons">
+          <article class="OverOnsArticle">
+          <h2 class="OverOnsH">Eco-vriendelijkheid</h2>
+            <p class="OverOnsP">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus omnis sapiente architecto eius, dicta totam. Ipsam beatae delectus vero eius, repellat asperiores iusto pariatur officiis. Aliquam dolores laborum libero molestias! Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus omnis sapiente architecto eius, dicta totam. Ipsam beatae delectus vero eius, repellat asperiores iusto pariatur officiis. Aliquam dolores laborum libero molestias! Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus omnis sapiente architecto eius, dicta totam. Ipsam beatae delectus vero eius, repellat asperiores iusto pariatur officiis. Aliquam dolores laborum libero molestias! Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus omnis sapiente architecto eius, dicta totam. Ipsam beatae delectus vero eius, repellat asperiores iusto pariatur officiis. Aliquam dolores laborum libero molestias! Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus omnis sapiente architecto eius, dicta totam. Ipsam beatae delectus vero eius, repellat asperiores iusto pariatur officiis. Aliquam dolores laborum libero molestias! </p>          
+          </article>
+        </section>
+        <section class="overons">
+          <article class="OverOnsArticle">
+          <h2 class="OverOnsH">Geschiedenis</h2>
+            <p class="OverOnsP">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus omnis sapiente architecto eius, dicta totam. Ipsam beatae delectus vero eius, repellat asperiores iusto pariatur officiis. Aliquam dolores laborum libero molestias! Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus omnis sapiente architecto eius, dicta totam. Ipsam beatae delectus vero eius, repellat asperiores iusto pariatur officiis. Aliquam dolores laborum libero molestias! Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus omnis sapiente architecto eius, dicta totam. Ipsam beatae delectus vero eius, repellat asperiores iusto pariatur officiis. Aliquam dolores laborum libero molestias! Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus omnis sapiente architecto eius, dicta totam. Ipsam beatae delectus vero eius, repellat asperiores iusto pariatur officiis. Aliquam dolores laborum libero molestias! Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus omnis sapiente architecto eius, dicta totam. Ipsam beatae delectus vero eius, repellat asperiores iusto pariatur officiis. Aliquam dolores laborum libero molestias! </p>          
+          </article>
+        </section>
+        <section class="overons">
+          <article class="OverOnsArticle">
+          <h2 class="OverOnsH">Doelstellingen</h2>
+            <p class="OverOnsP">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus omnis sapiente architecto eius, dicta totam. Ipsam beatae delectus vero eius, repellat asperiores iusto pariatur officiis. Aliquam dolores laborum libero molestias! Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus omnis sapiente architecto eius, dicta totam. Ipsam beatae delectus vero eius, repellat asperiores iusto pariatur officiis. Aliquam dolores laborum libero molestias! Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus omnis sapiente architecto eius, dicta totam. Ipsam beatae delectus vero eius, repellat asperiores iusto pariatur officiis. Aliquam dolores laborum libero molestias! Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus omnis sapiente architecto eius, dicta totam. Ipsam beatae delectus vero eius, repellat asperiores iusto pariatur officiis. Aliquam dolores laborum libero molestias! Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus omnis sapiente architecto eius, dicta totam. Ipsam beatae delectus vero eius, repellat asperiores iusto pariatur officiis. Aliquam dolores laborum libero molestias! </p>          
+          </article>
+        </section>
+        <section class="overons">
+          <h2>Wie werken er bij ons</h2>
+          <article class="OverOnsArticle">
+    <h2>Diego Ballestero Rietveldt</h2>
+    <div>
+    <p>According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.</p>
+  <img src="img/kummma.jpeg" alt="">
+  </div>
+  </article>
+  <article class="Aryan, OverOnsArticle">
+    <h2>Aryan Bhagwat</h2>
+    <div>
+    <p>According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.</p>
+  <img src="img/kummma.jpeg" alt="">
+  </div>
+  </article>
+  <article class="Yasin, OverOnsArticle">
+    <h2>Yasin Coban</h2>
+    <div>
+    <p>According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.</p>
+  <img src="img/kummma.jpeg" alt="">
+  </div>
+  </article>
+  <article class="Daniel, OverOnsArticle">
+    <h2>Daniel Kuipers</h2>
+    <div>
+    <p>According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.</p>
+  <img src="img/kummma.jpeg" alt="">
+  </div>
+  </article>
+  <article class="Berkay, OverOnsArticle">
+    <h2>Berkay Onal</h2>
+    <div>
+    <p>According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.</p>
+  <img src="img/kummma.jpeg" alt="">
+  </div>
+  </article>
+        </section>
+      </main>
+<!-- <section class="overons">
   <div class="Diego">
     <h2>Diego Ballestero Rietveldt</h2>
     <p>According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.</p>
@@ -33,7 +91,7 @@
   <div class="Berkay">
     <h2>Berkay Onal</h2>
     <p>According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.</p>
-  <img src="img/kummma.jpeg" alt="">
+  <img src="img/kummma.jpeg" alt=""> -->
   </div>
 </section>
 </body>
