0aca769762a86ab4535aa44614bee7f413836f74
danielkuiper1 <9015587@student.tcrmbo.nl>
2023-03-17 13:52:22 +0100

    Merge ofzo

--- project3/overons.php
+++ project3/overons.php
@@ -67,31 +67,6 @@
   </article>
         </section>
       </main>
-<!-- <section class="overons">
-  <div class="Diego">
-    <h2>Diego Ballestero Rietveldt</h2>
-    <p>According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.</p>
-  <img src="img/kummma.jpeg" alt="">
-  </div>
-  <div class="Aryan">
-    <h2>Aryan Bhagwat</h2>
-    <p>According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.</p>
-    <img src="img/kummma.jpeg" alt="">
-  </div>
-  <div class="Yasin">
-    <h2>Yasin Coban</h2>
-    <p>According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.</p>
-    <img src="img/kummma.jpeg" alt="">
-  </div>
-  <div class="Daniel">
-    <h2>Daniel Kuipers</h2>
-    <p>According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.</p>
-    <img src="img/kummma.jpeg" alt="">
-  </div>
-  <div class="Berkay">
-    <h2>Berkay Onal</h2>
-    <p>According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.</p>
-  <img src="img/kummma.jpeg" alt=""> -->
   </div>
 </section>
 </body>
