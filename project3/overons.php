957e5a0e548bd12ac7ea779633f134d7d97a78d0
danielkuiper1 <9015587@student.tcrmbo.nl>
2023-03-22 11:33:38 +0100

    Over Ons Eco-Vriendelijkheid

--- project3/overons.php
+++ project3/overons.php
@@ -13,7 +13,27 @@
         <section class="overons">
           <article class="OverOnsArticle">
           <h2 class="OverOnsH">Eco-vriendelijkheid</h2>
-            <p class="OverOnsP">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus omnis sapiente architecto eius, dicta totam. Ipsam beatae delectus vero eius, repellat asperiores iusto pariatur officiis. Aliquam dolores laborum libero molestias! Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus omnis sapiente architecto eius, dicta totam. Ipsam beatae delectus vero eius, repellat asperiores iusto pariatur officiis. Aliquam dolores laborum libero molestias! Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus omnis sapiente architecto eius, dicta totam. Ipsam beatae delectus vero eius, repellat asperiores iusto pariatur officiis. Aliquam dolores laborum libero molestias! Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus omnis sapiente architecto eius, dicta totam. Ipsam beatae delectus vero eius, repellat asperiores iusto pariatur officiis. Aliquam dolores laborum libero molestias! Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus omnis sapiente architecto eius, dicta totam. Ipsam beatae delectus vero eius, repellat asperiores iusto pariatur officiis. Aliquam dolores laborum libero molestias! </p>          
+            <p class="OverOnsP">Duurzaam en verantwoord leven wordt steeds belangrijker en is inmiddels bijna niet meer weg te denken uit ons dagelijks leven. Ook de mode-industrie zet zich hard in om haar stempel op het milieu zo 'groen' mogelijk te maken, want ook hier is duurzaamheid aan de orde van de dag. Duurzaamheid beschermt onze natuurlijke omgeving en de ecologische en menselijke gezondheid. We moeten milieubewuster worden, zodat onze mini-me's en hun kinderen ook nog een fijne wereld hebben om op te leven. We proberen dit al te doen door het gebruik van groene stroom, het scheiden van afval en bijvoorbeeld het rijden in elektrische auto's. Maar er is meer dan dat! Ook bij de productie van schoenen wordt er door de jaren heen steeds meer verduurzaamd. De duurzaamheid van een schoen hangt voor een groot deel af van de materialen waarvan deze is gemaakt, en de impact hiervan op het milieu. Milieuvriendelijke materialen en productieprocessen zijn daarom een must. Wij lichten een aantal duurzame materialen voor je uit.</p></br>
+            <h2>Leer</h2>
+            <div>
+            <p>Duurzaam leer wordt gemaakt van leer dat anders zou zijn weggegooid na het productieproces. Ook wordt dit leer gelooid zonder zware chemicaliën. Leer gaat ook nog eens lekker lang mee omdat je het goed kunt onderhouden en het dus niet makkelijk slijt!</p>
+            </div></br>
+            <h2>Textiel</h2>
+            <div>
+            <p>Hiervoor wordt er steeds meer gebruik gemaakt van gerecyclede materialen, zoals je oude cola fles (PET flessen). extielstof van gerecycled materiaal is extra stevig, en zeer duurzaam omdat er op deze manier plastic afval uit de oceaan beetje bij beetje verdwijnt. Maar ook biologisch katoen is super eco-friendly. Dit zorgt namelijk voor minder CO2 uitstoot en waterverspilling. Ook voor de boeren op de plantages zorgt biologische katoenproductie voor betere arbeidsomstandigheden.</p>
+            </div></br>
+            <h2>Kurk</h2>
+            <div>
+            <p>Kurk wordt voornamelijk gebuikt in zolen van schoenen. Kurk wordt verkregen uit de schors van de kurkeik en is 100% natuurlijk, biologisch afbreekbaar, milieuvriendelijk en recyclebaar. Daarnaast is kurk zeer schokabsorberend en veerkrachtig, waardoor schoenen met een kurk zool erg comfortabel lopen.</p>
+            </div></br>
+            <h2>Rubber</h2>
+            <div>
+            <p>Ook rubber kan zeer duurzaam zijn. Denk aan het recyclen ervan of aan rubber afkomstig van plantages die op een verantwoorde manier met grondbeheer omgaan. Ook bestaat er natuurrubber, dat wordt verkregen uit het sap van de zogeheten 'rubberbomen'. Doordat rubber niet makkelijk slijt, gaan je schoenen met een rubberen zool dus ook extra lang mee.</p>            
+            </div></br>
+            <h2>Schuim</h2>
+            <div>
+            <p>Vooral bij sneakers is de binnenzool gemaakt van comfortabel schuim. Tegenwoordig kan dit ook worden gemaakt op basis van suikerriet. De suikerriet zolen zijn een van de meest innovatieve en milieubewuste uitvindingen op modegebied van dit moment. De schuimzolen op basis van suikerriet dragen namelijk niet bij aan ontbossing van regenwouden, en maken het lopen zeer comfortabel. Daarnaast is het biologisch afbreekbaar!</p>            
+           </div>
           </article>
         </section>
         <section class="overons">
