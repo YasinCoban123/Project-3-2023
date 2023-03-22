4b52c79d2c051a728e3983683fcd769bc7e1d5f7
YasinCoban123 <9018681@student.zadkine.nl>
2023-03-22 11:27:48 +0100

    Schoen pagina work in progress

--- project3/schoenen.php
+++ project3/schoenen.php
@@ -9,11 +9,89 @@
 </head>
 <body>
   <?php include 'navbar.php'; ?>
-<aside class="asidefilter">
-  <h2>Filter</h2>
-  <p>Merk:</p>
-</aside>
+  <section>
+		<aside>
+			<h2>Filters</h2>
+			<label for="brand">Brand</label>
+			<select id="brand" name="brand">
+				<option value="all">All</option>
+				<option value="nike">Nike</option>
+				<option value="adidas">Adidas</option>
+				<option value="puma">Puma</option>
+			</select>
+			<label for="color">Color</label>
+			<select id="color" name="color">
+				<option value="all">All</option>
+				<option value="red">Red</option>
+				<option value="blue">Blue</option>
+				<option value="green">Green</option>
+			</select>
+			<label for="price">Price Range</label>
+			<input type="range" id="price" name="price" min="0" max="1000">
+			<span id="price-value"></span>
+			<button>Apply Filters</button>
+		</aside>
+		
 
+		<main>
+			<ul class="products">
+				<li>
+					<img src="img/nikesockdartprem.webp">
+					<h3>Product 1</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod magna at tristique vehicula.</p>
+					<span>$50.00</span>
+				</li>
+				<li>
+					<img src="img/nikeairmaxalpha.webp">
+					<h3>Product 2</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod magna at tristique vehicula.</p>
+					<span>$75.00</span>
+				</li>
+				<li>
+					<img src="img/nikeairmaxsystm.webp">
+					<h3>Product 3</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod magna at tristique vehicula.</p>
+					<span>$100.00</span>
+				</li>
+				<li>
+					<img src="img/nikesockdartprem.webp">
+					<h3>Product 1</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod magna at tristique vehicula.</p>
+					<span>$50.00</span>
+				</li><li>
+					<img src="img/nikesockdartprem.webp">
+					<h3>Product 1</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod magna at tristique vehicula.</p>
+					<span>$50.00</span>
+				</li><li>
+					<img src="img/nikesockdartprem.webp">
+					<h3>Product 1</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod magna at tristique vehicula.</p>
+					<span>$50.00</span>
+				</li><li>
+					<img src="img/nikesockdartprem.webp">
+					<h3>Product 1</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod magna at tristique vehicula.</p>
+					<span>$50.00</span>
+					<li>
+						<img src="img/nikesockdartprem.webp">
+						<h3>Product 1</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod magna at tristique vehicula.</p>
+						<span>$50.00</span>
+						<li>
+							<img src="img/nikesockdartprem.webp">
+							<h3>Product 1</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod magna at tristique vehicula.</p>
+							<span>$50.00</span>
+							<li>
+								<img src="img/nikesockdartprem.webp">
+								<h3>Product 1</h3>
+								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod magna at tristique vehicula.</p>
+								<span>$50.00</span>
+					
+			</ul>
+		</main>
+	</section>
 <?php include 'footer.php'; ?>
 </body>
 </html>
