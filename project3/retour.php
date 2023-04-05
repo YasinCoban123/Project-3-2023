5e8dd265104d3b19fe170710f01cb1e431f8d9d6
YasinCoban123 <9018681@student.zadkine.nl>
2023-04-05 10:43:11 +0200

    favicon geplaatst

--- project3/retour.php
+++ project3/retour.php
@@ -4,6 +4,7 @@
 <head>
     <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
+    <link rel="icon" href="img/schoenreus logo.png" type="image/x-icon">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Over ons</title>
