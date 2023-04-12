c178572e9b7dd06eb5c8928b2d6ed055a30cfff1
trueeman <9019468@student.zadkine.nl>
2023-04-12 23:51:55 +0200

    balls

--- project3/home_page.php
+++ project3/home_page.php
@@ -17,7 +17,9 @@
     <source src="img/achtergrondvideo.ogv" type="video/ogg">
 	</video>
   -->
-  
+
+
+  <!-- 
   <div class="schoen_preview">
     <div class="preview1">
       <img src="img/nikeairmaxalpha.png" class="nike_air_max_alpha">
@@ -26,7 +28,7 @@
     </div>
 
     <div class="preview2">
-      <img src="img/nikeairmaxsystm.png" class="nike_air_max_system">
+      <img src="img/nikeairmaxalpha.png" class="nike_air_max_alpha">
         <p>Nike Air Max Grey Blue</p>
         <button><a href="">Koop hier</a></button>
     </div>
@@ -39,7 +41,76 @@
 
       <h1 id="recommendation_tekst">Producten die we aanraden.</h1>
   </div>    
-    
+-->
+
+<!--
+<div class="home_slideshow">
+  <img src="img/nikeairmaxalpha.png" class="schoen_slides">
+  <img src="img/nikeairmaxsystm.png" class="schoen_slides">
+  <img src="img/nikesockdartprem.png" class="schoen_slides">
+</div>
+-->
+<div class="schoen_preview">
+<div class="slideshow-container">
+
+<div class="my_slides fade">
+  <img src="img/nikeairmaxalpha.png" class="schoen_slides">
+  <div class="tekst">Nike Air Max Alpha Smoke Grey</div>
+  <button><a href="schoen2.php">Koop hier</a></button>
+</div>
+
+<div class="my_slides fade">
+  <img src="img/nikeairmaxsystm.png" class="schoen_slides">
+  <div class="tekst">Nike Air Max Grey Blue</div>
+  <button><a href="schoen3.php">Koop hier</a></button>
+</div>
+
+<div class="my_slides fade">
+  <img src="img/nikesockdartprem.png" class="schoen_slides">
+  <div class="tekst">Nike Sock Dart Premium Orange</div>
+  <button><a href="schoen1.php">Koop hier</a></button>
+</div>
+
+<a class="prev" onclick="plusSlides(-1)">❮</a>
+<a class="next" onclick="plusSlides(1)">❯</a>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+</div>
+</div>
+
+<script>let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("my_slides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}</script>
+  
 <?php include 'footer.php'; ?>
 <audio id="backgroundtrack" src="mussic.mp3"></audio>
 <script src="script.js"></script>
--- project3/navbar.php
+++ project3/navbar.php
@@ -7,14 +7,15 @@
   <title>Nav</title>
 </head>
 <body>
-  <a href="home_page.php"><img id="logo" src="img/schoenreus logo.png" alt=""></a>
+  <a href="home_page.php"><img class="logo" src="img/schoenreus logo.png" alt=""></a>
+  <a href="winkelwagen.php"><img class="mandje" src="img/winkelwagenicoon2.png" alt=""></a>
   <nav>
     <ul>
         <li><a href="home_page.php">Home</a></li>
         <li><a href="schoenen.php">Schoenen</a></li>
         <li><a href="overons.php">Over Ons</a></li>
         <li><a href="contact.php">Contact</a></li>
-        <li><a href="retour.php">Retourneren</a></li>
+        <li><a href="retour.php" id="eind_nav">Retourneren</a></li>
     </ul>
   </nav>
 </body>
