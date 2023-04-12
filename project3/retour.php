1bb52a2eb01f255c927ff62370a5b9e7d4272053
trueeman <9019468@student.zadkine.nl>
2023-04-13 00:01:16 +0200

    hehe

--- project3/retour.php
+++ project3/retour.php
@@ -14,11 +14,11 @@
         <article class="OverOnsArticle">
             <div class="retour_info">
             <h2 class="OverOnsH">SchoenReus Retourservice</h2>
-                <p class="OverOnsP">De bol.com retourservice biedt je een geautomatiseerd retourproces met
+                <p class="retour_p">De bol.com retourservice biedt je een geautomatiseerd retourproces met
                 lagere retourkosten per pakket.</p> </br> <br>
             <h2>SchoenReus vergoedt deels de retourkosten</h2>
             <div>
-                <p class="OverOnsP">Klanten kunnen de artikelen die ze kopen via bol.com altijd gratis 
+                <p class="retour_p">Klanten kunnen de artikelen die ze kopen via bol.com altijd gratis 
                 retourneren. Echter zitten hier voor jou wel kosten aan verbonden en daarin willen we je graag tegemoet komen. Daarom bieden we een retourlabelservice aan, waarmee we de retourkosten deels vergoeden. Je schaft met deze service namelijk retourlabels aan voor brievenbuspakjes en pakketten tegen een voordelig tarief.
                 Let op! Gebruik maken van de bol.com retourservice is alleen mogelijk als de artikelen 
                 voldoen aan de afmetingen van de verzenddienst. Bij artikelen die te groot of te zwaar 
